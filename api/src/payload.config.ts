036922a722873941852a19292263d18ab221ad10
David Hernández <hello@davidhernandez.info>
2023-12-08 15:45:55 +0100

    Adds vite bundler

--- api/src/payload.config.ts
+++ api/src/payload.config.ts
@@ -1,11 +1,16 @@
 import { buildConfig } from 'payload/config'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { viteBundler } from '@payloadcms/bundler-vite'
 
 const { MONGODB_USER, MONGODB_PASSWORD } = process.env
 const url = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@db:27017/`
 
 export default buildConfig({
+    admin: {
+        bundler: viteBundler({}),
+    },
+    cors: '*',
     db: mongooseAdapter({
         url,
     }),
